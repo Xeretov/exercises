c9ea41b9442f75e172ed7e66f74ff484cb1b10a1
Xeretov <gioeleamendola@gmail.com>
2024-05-08 13:35:14 +0200

    Added lezione6 to complete

---
+++ Lezione6/lezione6.py
@@ -0,0 +1,11 @@
+# Gioele Amendola
+# 08/05/2024
+
+# Python classes:
+
+# Create a Class that represents a Person
+# first_name, last_name, ssn
+# birth_date, birth_place, gender
+
+# Define all getters and setters
+# Define a function that calculates ssn (and update for every possible change)
