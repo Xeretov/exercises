57d053e35c162158d2322e1cff7516380ec35ba1
Xeretov <gioeleamendola@gmail.com>
2024-05-14 11:57:17 +0200

    added weather conditions for lepre_tartaruga

--- Project_Rabbit_and_Turtle/lepre_tartaruga.py
+++ Project_Rabbit_and_Turtle/lepre_tartaruga.py
@@ -4,9 +4,10 @@ This module provides several functions to simulate a race between the turtle and
 
 # Gioele Amendola
 # 14/05/2024
+
 from random import choices
 
-def turtle_walk_speed() -> int:
+def turtle_walk_speed(weather: bool = False) -> int:
     '''
     This function returns the walking speed of the turtle based on probability.
 
@@ -15,9 +16,9 @@ def turtle_walk_speed() -> int:
     '''
     walking_speed: list[int] = [3, 1, -6]
     weight: list[float] = [0.5, 0.3, 0.2]
-    return choices(walking_speed, weight)[0]
+    return choices(walking_speed, weight)[0] - 1 if weather else choices(walking_speed, weight)[0] 
 
-def hare_walk_speed() -> int:
+def hare_walk_speed(weather: bool = False) -> int:
     '''
     This function returns the walking speed of the hare based on probability
 
@@ -26,7 +27,7 @@ def hare_walk_speed() -> int:
     '''
     walking_speed: list[int] = [0, 9, -12, 1, -2]
     weight: float = [0.2, 0.2, 0.1, 0.3, 0.2]
-    return choices(walking_speed, weight)[0]
+    return choices(walking_speed, weight)[0] -2 if weather else choices(walking_speed, weight)[0] 
 
 def show_route(route: list[str]) -> None:
     '''
@@ -42,15 +43,19 @@ def start_simulation() -> None:
     '''
     route: list[str] = ["-"]*70
     i: int = 1
-    racing: bool = True
+    weather: bool = choices([True,False],[0.5,0.5])[0]
     t_token: int = 0
     h_token: int = 0
+    print("\nIt's raining ☂" if weather else "\nIt's sunny ☀︎")
     print("\nBANG !!!!! AND THEY'RE OFF !!!!!\n")
-    while racing:
+    while True:
         route[t_token] = "-"
         route[h_token] = "-"
-        t_token += turtle_walk_speed()
-        h_token += hare_walk_speed()
+        if i % 10 == 0:
+            weather = choices([True,False],[0.5,0.5])[0]
+            print("It's raining ☂" if weather else "It's sunny ☀︎",end="\n\n")
+        t_token += turtle_walk_speed(weather)
+        h_token += hare_walk_speed(weather)
         if t_token >= 70 and h_token >= 70:
             route[69] = "X"
             print("Last Lap:")
