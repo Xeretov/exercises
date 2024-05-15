c178034eb4bcd2888b5bc51d169291a4aa217a9f
Xeretov <gioeleamendola@gmail.com>
2024-05-15 09:41:54 +0200

    removed the test and finished the code for gestione_dello_zoo_park

--- Project_Zoo/gestione_dello_zoo_park.py
+++ Project_Zoo/gestione_dello_zoo_park.py
@@ -88,11 +88,11 @@ class ZooKeeper:
                     fence.animals.append(animal)
                     animal.fence = fence
                 else:
-                    raise Exception(f"The animal {animal.name} cannot be contained in the specified fence.")
+                    print(f"\nThe animal {animal.name} cannot be contained in the specified fence.")
             else:
-                raise Exception(f"The animal {animal.name}'s preferred habitat '{animal.preferred_habitat}' is not the same as the fence's '{fence.habitat}'.")
+                print(f"\nThe animal {animal.name}'s preferred habitat '{animal.preferred_habitat}' is not the same as the fence's '{fence.habitat}'.")
         else:
-            raise Exception(f"The animal {animal.name} is already inside the fence {animal.fence.habitat}.")
+            print(f"\nThe animal {animal.name} is already inside the fence {animal.fence.habitat}.")
 
     def remove_animal(self, remove_animal: Animal, fence: Fence) -> None:
         '''
@@ -110,9 +110,9 @@ class ZooKeeper:
                 del animals[index]
                 remove_animal.fence = None
             else:
-                raise Exception(f"There is no animal by the name of {remove_animal.name} in the fence.")
+                print(f"\nThere is no animal by the name of {remove_animal.name} in the fence {fence.habitat}.")
         else:
-            raise Exception("The fence is empty.")
+            print("\nThe fence is empty.")
     
     def feed(self, feed_animal: Animal) -> None:
         '''
@@ -123,13 +123,13 @@ class ZooKeeper:
         '''
         if feed_animal.fence:
             if feed_animal.fence.is_available(feed_animal, feed=True):
-                feed_animal.width += feed_animal.width*2/100
-                feed_animal.height += feed_animal.height*2/100
+                feed_animal.width += feed_animal.width/50
+                feed_animal.height += feed_animal.height/50
                 feed_animal.health += feed_animal.health/100
             else:
-                raise Exception("The animal will get too big if fed.")
+                print(f"\nThe animal {feed_animal.name} will get too big if fed.")
         else:
-            raise Exception("The animal doesn't belong to any fence")
+            print(f"\nThe animal {feed_animal.name} doesn't belong to any fence")
     
     def clean(self, fence: Fence) -> float:
         '''
@@ -167,42 +167,3 @@ class Zoo:
                     print(f"Animal(name={animal.name}, species={animal.species}, age={animal.age})",end="\n\n")
             print("#"*30)
         print(end="\n\n")
-
-# Example Test:
-animal1: Animal = Animal("Harambe", "Western Gorilla", 17, 2.1, 0.5, "Tropical")
-animal2: Animal = Animal("Fiona", "Common Hippopotamus", 20, 1.6, 1.2, "Savannah")
-animal3: Animal = Animal("Diego", "Aldabra giant tortoise", 90, 0.8, 1.2, "Tropical")
-animal4: Animal = Animal("Winter", "Hourglass Dolphin", 28, 0.2, 1.9, "Antarctic")
-animal5: Animal = Animal("Pingu", "Emperor penguin", 8, 0.5, 0.18, "Antarctic")
-
-animal_list = [animal1, animal2, animal3]
-
-fence1: Fence = Fence(10000, 24, "Tropical", animal_list)
-fence2: Fence = Fence(50000, -10, "Antarctic")
-
-fence_list = [fence1,fence2]
-
-zookeeper1: ZooKeeper = ZooKeeper("Giacomo","Belli","120D58A")
-zookeeper2: ZooKeeper = ZooKeeper("Liliana","Fortuna","3982BL4")
-zookeeper3: ZooKeeper = ZooKeeper("Priscilla","Perlana","032AA27")
-
-zookeeper_list = [zookeeper1,zookeeper2,zookeeper3]
-
-zoo1: Zoo = Zoo(fence_list, zookeeper_list)
-
-zoo1.describe_zoo()
-
-zookeeper1.add_animal(animal4, fence2)
-# zookeeper1.add_animal(animal2, fence2)
-zookeeper1.remove_animal(animal1, fence1)
-# zookeeper1.remove_animal(animal5, fence2)
-zookeeper1.add_animal(animal5, fence2)
-
-zoo1.describe_zoo()
-
-# zookeeper2.feed(animal5, fence1)
-for _ in range(10):
-    zookeeper2.feed(animal3)
-
-print(f"{zookeeper3.clean(fence2)}")
-print(f"{zookeeper3.clean(fence1)}")
