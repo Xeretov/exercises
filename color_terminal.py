325e2b861bc86eab2ff1d0d6d1cbbc8e33a11d45
Xeretov <gioeleamendola@gmail.com>
2024-05-28 21:29:07 +0200

    Creation of color_terminal.py * Adds variables with strings to change print color

---
+++ color_terminal.py
@@ -0,0 +1,20 @@
+'''
+This module provides variables with the color code for the terminal    
+'''
+clear: str = "\x1b[0m"
+bold: str = "\x1b[1m"
+black: str = "\x1b[30m"
+red: str = "\x1b[31m"
+green: str = "\x1b[32m"
+yellow: str = "\x1b[33m"
+blue: str = "\x1b[34m"
+magenta: str = "\x1b[35m"
+cyan: str = "\x1b[36m"
+gray: str = "\x1b[90m"
+grey: str = "\x1b[90m"
+light_red: str = "\x1b[91m"
+light_green: str = "\x1b[92m"
+light_yellow: str = "\x1b[93m"
+light_blue: str = "\x1b[94m"
+light_magenta: str = "\x1b[95m"
+light_cyan: str = "\x1b[96m"
