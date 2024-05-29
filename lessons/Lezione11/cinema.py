45d70ea58ce50bae5a4d016f84e8c3ed67c9ecad
Xeretov <gioeleamendola@gmail.com>
2024-05-29 20:39:58 +0200

    finish cinema.py

--- lessons/Lezione11/cinema.py
+++ lessons/Lezione11/cinema.py
@@ -9,38 +9,51 @@
 class MovieCatalog:
 
     def __init__(self) -> None:
-        self.directors: dict = {}
+        self.catalog: dict[str:list] = {}
     
     def add_movie(self, director_name: str, movies: list[str]|str) -> None:
+        director_name = director_name.lower()
         if isinstance(movies, str):
-            movies = list(movies)
-        if director_name in self.directors:
-            self.directors[director_name].append(movies)
+            movies = [movies.lower()]
+        if director_name in self.catalog:
+            self.catalog[director_name] += [movie.lower() for movie in movies]
         else:
-            self.directors[director_name] = movies
+            self.catalog[director_name] = [movie.lower() for movie in movies]
     
     def remove_movie(self, director_name: str, movie_name: str) -> None:
-        if director_name in self.directors:
-            if movie_name in self.directors[director_name]:
-                self.directors[director_name].remove(movie_name)
-        if not self.directors[director_name]:
-            del self.directors[director_name]
+        director_name = director_name.lower()
+        movie_name = movie_name.lower()
+        if director_name in self.catalog:
+            if movie_name in self.catalog[director_name]:
+                self.catalog[director_name].remove(movie_name)
+        if not self.catalog[director_name]:
+            del self.catalog[director_name]
 
     def list_directors(self) -> None:
         print()
-        for director in self.directors:
+        for director in self.catalog:
             print("director: "+director)
         print()
     
     def get_movies_by_director(self, director_name: str) -> list[str]:
-        if director_name in self.directors:
-            return self.directors[director_name]
+        director_name = director_name.lower()
+        if director_name in self.catalog:
+            return self.catalog[director_name]
     
-    def search_movies_by_title(self, title: str) -> dict[str:list]:
-        result: dict = {}
-        for director in self.directors:
-            lista = self.directors[director]
-            result[director] = [].append(title) if title in lista else None
+    def search_movies_by_title(self, title: str) -> list[str]:
+        result: list = []
+        title = title.lower()
+        for director in self.catalog:
+            movies: list[str] = self.catalog[director]
+            for movie in movies:
+                if title in movie:
+                    if director not in result:
+                        result.append(director)
+                    result.append(movie)
+        if result:
+            return result
+        return f"No movie title with {title} in it was found"
+                    
         
 
 #     - search_movies_by_title(title): Trova tutti i film che contengono una certa parola nel titolo. Restituisce un elenco dei registi e dei rispettivi film che contengono la parola cercata o un messaggio di errore se nessun film contiene la parola cercata nel titolo.
@@ -59,4 +72,5 @@ if __name__ == "__main__":
     catalog.remove_movie("Christopher Nolan","Inception")
     catalog.remove_movie("Christopher Nolan","Oppenheimer")
     catalog.list_directors()
+    print(catalog.get_movies_by_director("Quentin Tarantino"))
     print(catalog.search_movies_by_title("The"))
