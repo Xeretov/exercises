f08b243cb94d1d9fcb997d5f6ca0f6d370446b49
Xeretov <gioeleamendola@gmail.com>
2024-05-31 12:30:32 +0200

    improve leetCode block_placement_queries.py

--- leetCode/block_placement_queries.py
+++ leetCode/block_placement_queries.py
@@ -20,22 +20,17 @@ def block_queries(queries: list[list[int]]) -> list[bool]:
     blocks: list[int] = [0]
     result: list[bool] = []
     for query in queries:
-        if len(query) == 2:
-            not_append: bool = False
-            index: int = 0
-            for i,x in enumerate(blocks):
-                if query[1] < x:
-                    not_append = True
-                    index = i
-                    break
-            if not_append:
-                blocks.insert(index,query[1])
-            else:
-                blocks.append(query[1])
+        if query[0] == 1:
+            blocks.append(query[1])
+            if blocks[-1] < blocks[-2]:
+                blocks.sort()
         else:
-            before: list = [x for x in blocks if x<=query[1]]
-            i, prev = before[-1], before[-2]
-            max_size = i-prev
+            if query[1] < query[2]:
+                result.append(False)
+                continue
+            check: list = [x for x in blocks if x <= query[1]]
+            check.append(query[1])
+            max_size = max(x - check[i] for i, x in enumerate(check[1:]))
             if max_size >= query[2]:
                 result.append(True)
             else:
