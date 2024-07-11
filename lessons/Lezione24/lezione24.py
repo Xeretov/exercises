d127f481cef213f94e1c0295dcaacd02ec8bffb9
xeretov <gioeleamendola@gmail.com>
2024-07-11 10:16:36 +0200

    Add unittest cases in lezione24

--- lessons/Lezione24/lezione24.py
+++ lessons/Lezione24/lezione24.py
@@ -12,7 +12,7 @@ class Documento:
         self.__testo = testo
 
     def isInText(self, key: str) -> bool:
-        return key in self.__testo
+        return key.lower() in self.__testo.lower()
 
 class Email(Documento):
     def __init__(self, testo: str, mittente: str, destinatario: str, titolo: str) -> None:
@@ -58,6 +58,10 @@ class File(Documento):
     def getTesto(self) -> str:
         return super().getText()
 
+    def scriviTestoInFile(self):
+        with open(self.nomePercorso, "w") as file:
+            file.write(self.getTesto())
+
     def leggiTestoDaFile(self):
         with open(self.nomePercorso, "r") as file:
             self.__testo = file.read()
@@ -68,13 +72,13 @@ class File(Documento):
 
 email: Email = Email("Hello,\n\nI am a Nigerian prince, I need your help to get my royal money.\nWith just 5000 euros I can access my billions, if you help me I can send you 100,000 euros.\n\nNice meeting you,\nThe nigerian prince.", "nigerianprince@gmail.com","gioeleamendola@gmail.com","Royal money")
 
-file: File = File("Ciao Mondo!","C:\\Users\\Cloud\\Documents\\ITIS\\VSProjects\\exercises\\lessons\\Lezione24\\documentoImportante\\princip.txt")
+file: File = File("Ciao Mondo!","lessons\\Lezione24\\documentoImportante\\princip.txt")
 
 print("\n"+email.getText(),end="\n\n")
 
 print("\n"+file.getText(),end="\n\n")
 
-email.writeToFile("email1.txt","C:\\Users\\Cloud\\Documents\\ITIS\\VSProjects\\exercises\\lessons\\Lezione24\\documentoImportante")
+email.writeToFile("email1.txt","lessons\\Lezione24\\documentoImportante")
 
 print("\nLa parola 'incontrarci' è presente nel testo dell'email? ",email.isInText("meet"),end="\n\n")
 
@@ -82,13 +86,51 @@ print("\nLa parola 'percorso' è presente nel testo del file?  ",file.isInText("
 
 # ### Test con UnitTest
 # Utilizzando il modulo unittest, definire i seguenti test per le classi Documento, Email e File.
+import unittest
+import os
+class TestDocumento(unittest.TestCase):
+    def setUp(self) -> None:
+        self.doc: Documento = Documento("Hello World!")
 
+    def test_getText(self):
+        self.assertEqual(self.doc.getText(),"Hello World!")
  
-# I test devono includere:
-# Verifica dei metodi getText() e setText() nella classe Documento.
-# Verifica del metodo isInText() nella classe Documento.
-# Verifica del metodo getText() nella classe Email.
-# Verifica del metodo writeToFile() nella classe Email.
-# Verifica del metodo isInText() nella classe Email.
-# Verifica del metodo getText() nella classe File.
-# Verifica del metodo isInText() nella classe File.
+    def test_setText(self):
+        self.doc.setText("Ciao Mondo!")
+        self.assertEqual(self.doc.getText(),"Ciao Mondo!")
+
+    def test_isInText(self):
+        self.assertEqual(self.doc.isInText("hello"),True)
+        self.assertEqual(self.doc.isInText("carretto"),False)
+
+class TestEmail(unittest.TestCase):
+    def setUp(self) -> None:
+        self.email: Email = Email("Hello everyone!\nI'm glad to announce...","promotion@blizzard.com","julianassange@wikileaks.com","Promotion")
+    
+    def test_getText(self) -> None:
+        self.assertEqual(self.email.getText(),"Da: promotion@blizzard.com, A: julianassange@wikileaks.com\nTitolo: Promotion\nMessaggio:\nHello everyone!\nI'm glad to announce...")
+    
+    def test_writeToFile(self) -> None:
+        self.email.writeToFile("email.txt","lessons\\Lezione24\\documentoImportante")
+        self.assertTrue(os.path.exists("lessons\\Lezione24\\documentoImportante\email.txt"))
+        with open("lessons\\Lezione24\\documentoImportante\\email.txt","r") as file:
+            self.assertEqual(file.read(),self.email.getText())
+        os.remove("lessons\\Lezione24\\documentoImportante\email.txt")
+    
+    def test_isInText(self) -> None:
+        self.assertEqual(self.email.isInText("everyone"),True)
+        self.assertEqual(self.email.isInText("carretto"),False)
+
+class TestFile(unittest.TestCase):
+    def setUp(self) -> None:
+        self.file: File = File("Hello World!","lessons\\Lezione24\\documentoImportante\\file.txt")
+    
+    def test_getText(self) -> None:
+        self.assertEqual(self.file.getText(),"Percorso: " + "lessons\\Lezione24\\documentoImportante\\file.txt" + "\nContenuto: " + "Hello World!")
+    
+    def test_isInText(self) -> None:
+        self.assertEqual(self.file.isInText("hello"),True)
+        self.assertEqual(self.file.isInText("carretto"),False)
+
+if __name__ == "__main__":
+    unittest.main()
